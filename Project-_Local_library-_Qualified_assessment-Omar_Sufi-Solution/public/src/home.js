031a45ed3c638e47feaba5df597eeab22d95d5ee
Omar Sufi <126733712+OmarSufi@users.noreply.github.com>
2023-03-19 19:30:21 -0500

    updated so it has reduce function

--- Project-_Local_library-_Qualified_assessment-Omar_Sufi-Solution/public/src/home.js
+++ Project-_Local_library-_Qualified_assessment-Omar_Sufi-Solution/public/src/home.js
@@ -15,23 +15,14 @@ function getBooksBorrowedCount(books) {
 }
 
 function getMostCommonGenres(books) {
- let map = {};
- books.forEach((num) => {
-  if (map[num.genre]) {
-   map[num.genre]++;
-  } else {
-   map[num.genre] = 1;
-  }
- });
- return Object.entries(map)
-  .map(([name, count]) => {
-   return {
-    name,
-    count
-   };
-  })
-  .sort((a, b) => b.count - a.count)
-  .slice(0, 5);
+  const genreName = books.map((book) => book.genre);
+  const genreMap = genreName.reduce((prev, cur) => { 
+    prev[cur] = (prev[cur] || 0) + 1; 
+    return prev; 
+  }, [])
+  const genreArr = Object.entries(genreMap).map(([name, count]) => ({name, count}));
+  const topGenres = genreArr.sort((ctPrev, ctCur) => ctCur.count - ctPrev.count).slice(0,5);
+  return topGenres;
 }
 
 function getMostPopularBooks(books) {
@@ -59,6 +50,8 @@ function getMostPopularAuthors(books, authors) {
  });
  return result.sort((a, b) => b.count - a.count).slice(0, 5);
 }
+
+
 
 module.exports = {
   getTotalBooksCount,
